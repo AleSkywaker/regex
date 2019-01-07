67cf98c200d2dd191895ed2b78388a842800d56a
Alex Colombo <uricaine@hotmail.com>
2019-01-07 22:54:29 +0100

    change

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ console.log(expression1.test("Alex"));
 let expression2 = /ale/;
 console.log(expression2.test("alex"));
 
-let expression3 = /[01azAz]/
+let expression3 = /[^01azAz]/
 
 console.log(expression3.test("al"))
