48d102a88e518885216b383e80b243c2d4d6c088
Alex Colombo <uricaine@hotmail.com>
2019-01-07 22:53:22 +0100

    change

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ console.log(expression1.test("Alex"));
 let expression2 = /ale/;
 console.log(expression2.test("alex"));
 
-let expression3 = /[a-1 a-z A-z]/
+let expression3 = /[a-1a-zA-z]/
 
 console.log(expression3.test("$$$"))
