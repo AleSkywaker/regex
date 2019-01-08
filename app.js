c1cb2b93531039029931d102f5de6b66ee607699
Alex Colombo <uricaine@hotmail.com>
2019-01-08 22:11:51 +0100

    new expression

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 let expression1 = new RegExp("abc");
 
-console.log(expression1.test("Alex"));
+console.log("expression1", expression1.test("Alex"));
 
 let expression2 = /ale/;
-console.log(expression2.test("alex"));
+console.log("expression2", expression2.test("alex"));
 
 let expression3 = /[01azAz]/
 
-console.log(expression3.test("€€€"))
+console.log("expression3", expression3.test("€€€"))
 
 let expression4 = /\d/;
-console.log(expression4.test("€€€"))
+console.log("expression4", expression4.test("€€€"))
 
 
