d3bfb8c62d561d36cdb02295bb422c9b6a6d39a4
Alex Colombo <uricaine@hotmail.com>
2019-01-09 20:24:34 +0100

    new expression

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ console.log("expression3", expression3.test("€€€"))
 
 let expression4 = /\d*/;  //el simbolo de asterisco indica que puedo no haber ningun digito,  o varios
 console.log("expression4", expression4.test(""))
-console.log("expression4", expression4.test(255555))
+console.log("expression4.1", expression4.test(255555))
 
 let expression5 = /\d+/;  //el simbolo de 'mas' indica que tiene que haber por lo menos un digito.
 console.log("expression5", expression5.test(""))
-console.log("expression5", expression5.test(255555))
+console.log("expression5.1", expression5.test(255555))
 
 let fecha = /\d\d-\d\d-\d\d\d\d/
 console.log("fecha", fecha.test('22-08-1975'))
