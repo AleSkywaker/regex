10c343fce0448faf8062965d137f5137996d72ad
Alex Colombo <uricaine@hotmail.com>
2019-01-10 21:05:01 +0100

    new expression

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 let expression1 = new RegExp("abc");
 
-console.log("expression1", expression1.test("Alex"));
-
 let expression2 = /ale/;
-console.log("expression2", expression2.test("alex"));
 
 let expression3 = /[01azAz]/
 
-console.log("expression3", expression3.test("€€€"))
 
 let expression4 = /\d*/;  //el simbolo de asterisco indica que puedo no haber ningun digito,  o varios
 console.log("expression4", expression4.test(""))
