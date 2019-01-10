a82782609822df7803007a9e6fcdafa06dcd9703
Alex Colombo <uricaine@hotmail.com>
2019-01-10 21:05:27 +0100

    new expression

--- app.js
+++ app.js
@@ -4,22 +4,13 @@ let expression2 = /ale/;
 
 let expression3 = /[01azAz]/
 
-
 let expression4 = /\d*/;  //el simbolo de asterisco indica que puedo no haber ningun digito,  o varios
-console.log("expression4", expression4.test(""))
-console.log("expression4.1", expression4.test(255555))
 
 let expression5 = /\d+/;  //el simbolo de 'mas' indica que tiene que haber por lo menos un digito.
-console.log("expression5", expression5.test(""))
-console.log("expression5.1", expression5.test(255555))
 
 let expression6 = /\d?/;  //el simbolo de 'interrogación' indica que el digito es opcional.
-console.log("expression6", expression6.test(""))
-console.log("expression6.1", expression6.test(255555))
 
 let fecha = /\d\d-\d\d-\d\d\d\d/
-console.log("fecha", fecha.test('22-08-1985'))
-
 
 let evaluacion = /Hola mun?do/
 console.log("evaluacion", evaluacion.test('Hola mudo'))
