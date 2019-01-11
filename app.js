75ae662522ada301bfe67205836c15bc3b6b30eb
Alex Colombo <uricaine@hotmail.com>
2019-01-11 22:03:06 +0100

    new expression

--- app.js
+++ app.js
@@ -13,10 +13,11 @@ let expression6 = /\d?/;  //el simbolo de 'interrogación' indica que el digito 
 let fecha = /\d\d-\d\d-\d\d\d\d/
 
 let evaluacion = /Hola mun?do/
-console.log("evaluacion", evaluacion.test('Hola mudo'))
 
 let expression7 = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{1,2}/
 console.log("expression7", expression7.test('27-06-2007 11:45'))
+
+
 
 
 
