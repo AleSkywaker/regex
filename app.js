ce0a17b2b623cd7c90f4af7b3ef8846e470bee14
Alex Colombo <uricaine@hotmail.com>
2019-01-16 20:03:59 +0100

    new wxpression

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ var mensaje = 'El cliente pidió sushi y comio su sushi';
 console.log(mensaje.replace(/sushi/g, "tacos")) 
 // metodo search devuelve la posicion de coincidencia
 // solo devuelve la primera coincidencia aunque uses el flag global
-console.log(mensaje.search(/\W/))
+console.log(mensaje.search(/\W/))//busca cualquier cosa que no sea caracter
 console.log(mensaje.search(/\s/))
 
